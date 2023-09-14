6be281c4e7685cc600f0bf021185befbc11ed1f2
Carlos Caltzontzin <cacr71993@yahoo.com>
2023-09-13 22:58:26 -0500

    completed phase-1-sending-data-with-fetch

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+function submitData(name, email) {
+    const userData = {
+      name: name,
+      email: email
+    };
+  
+    const configObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(userData)
+    };
+  
+    return fetch("http://localhost:3000/users", configObject)
+      .then(response => response.json())
+      .then(data => {
+        const userId = data.id;
+        const idElement = document.createElement("p");
+        idElement.textContent = userId;
+        document.body.appendChild(idElement);
+      })
+      .catch(error => {
+        const errorElement = document.createElement("p");
+        errorElement.textContent = error.message;
+        document.body.appendChild(errorElement);
+      });
+  }
+  
